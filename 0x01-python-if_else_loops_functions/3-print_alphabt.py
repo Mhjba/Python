b53b31371cdd5b08120ae9060cce98dccd145d4a
Mhjba <mahjoubaaitalla15@gmail.com>
2023-11-28 04:28:05 -0800

    python functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i != ord('q') and i != ord('e'):
-        print('{:c}'.format(i), end='')
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue
+    else:
+        print("{}".format(chr(i)), end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for x in range(10):
-        if i < x:
-            print('{}{}'.format(i, x), end='')
-            if i < 8:
-                print(', ', end='')
-print()
+for i in range(9):
+    for x in range(i + 1, 10):
+        if i < 8:
+            print("{:d}{:d}".format(i, x), end=", ")
+        else:
+            print("{:d}{:d}".format(i, x))
