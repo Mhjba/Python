14459fb97ee4c4c605f105a9ccaed6219c64e9da
Mhjba <mahjoubaaitalla15@gmail.com>
2023-11-28 06:01:05 -0800

    python functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if letter == 101 or letter == 113:
+for i in range(97, 123):
+    if i == 101 or i == 113:
         continue
     else:
-        print("{}".format(chr(letter)), end="")
+        print("{}".format(chr(i)), end="")
