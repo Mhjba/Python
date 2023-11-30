91f27baa62cef2a89f23df36b4334bc56209d483
Mhjba <mahjoubaaitalla15@gmail.com>
2023-11-30 03:58:35 -0800

    import modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
 
     i = len(sys.argv) - 1
     if i == 0:
-        print("0 arguments.")
+        print("0 arguments.".format(i))
     elif i == 1:
-        print("1 argument:")
+        print("1 argument:".format(i))
     else:
         print("{} arguments:".format(i))
         for x in range(i):
