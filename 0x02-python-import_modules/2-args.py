ff4d765fa168e692d4806ddf05794ec23e7a228f
Mhjba <mahjoubaaitalla15@gmail.com>
2023-11-30 03:47:44 -0800

    import modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(i))
-        for i in range(i):
-            print("{}: {}".format(i, sys.argv))
+        for x in range(i):
+            print("{}: {}".format(x, sys.argv))
