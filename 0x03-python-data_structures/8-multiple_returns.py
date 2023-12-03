3a2959b332532ac9249bc137b5209611bfde1dfa
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-03 08:22:08 -0800

    data structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-
     if len(sentence) == 0:
-        tup = (0, None)
+        return (None, None)
     else:
-        tup = (len(sentence), sentence[:1])
-    return(tup)
+        return (len(sentence), sentence[0])
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
 
-    if my_list == []:
-        return(None)
-    my_list.sort()
-    return(my_list[-1])
+    return max(my_list) if my_list else None
