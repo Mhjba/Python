54b39c7996ee6e0a3600a2dd3d1d77db325fe156
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-03 04:06:25 -0800

    data structures

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+
+    my_string_char = list(my_string)
+    for char in my_string_char:
+        if char == 'c' or char == 'C':
+            my_string_char.remove(char)
+            return "".join(my_string_char)
