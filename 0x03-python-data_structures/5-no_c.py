d9ca19d22fb36183183b0fb0c88cc12cfc429e64
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-03 04:44:13 -0800

    data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    my_string_char = list(my_string)
-    for char in my_string_char:
+    string_char = list(my_string)
+    for char in string_char:
         if char == 'c' or char == 'C':
-            my_string_char.remove(char)
-    return "".join(my_string_char)
+            string_char.remove(char)
+    return("".join(string_char))
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+
     if (len(tuple_a) == 0):
         tuple_a = (0, 0)
     elif (len(tuple_a) == 1):
@@ -8,5 +9,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = (0, 0)
     elif (len(tuple_b) == 1):
         tuple_b = (tuple_b[0], 0)
-
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
