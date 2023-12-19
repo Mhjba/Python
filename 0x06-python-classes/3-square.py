7c74b4e7e687bae0f697e0b15a6e5f8bf3d62f2e
Mhjba <mahjoubaaitalla15@gmail.com>
2023-12-19 07:11:47 -0800

    classes objactes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-"""Square module """
+
+"""Define a class Square."""
 
 
-class Square():
-    """Defines a square."""
+class Square:
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """constructor.
+        """Initialize a new square.
 
         Args:
-            size : length of a side of the square.
-
-        Raises:
-            TypeError: if size is not an integer.
-            ValueError: if size is less than 0.
+            size (int): The size of the new square.
         """
-        if type(size) is int:
-            if size >= 0:
-                self.__size = size
-            else:
-                raise ValueError("size must be >= 0")
-        else:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        """Returns the current square area."""
-        return self.__size ** 2i
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,41 +1,32 @@
 #!/usr/bin/python3
-"""Square module"""
+
+"""Define a class Square."""
 
 
 class Square:
-    """Class square .
-
-    Args:
-        size : length of a side of the square
-"""
+    """Represent a square."""
 
     def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     @property
     def size(self):
-        """__size getter, setter with same method name
-
-        Returns:
-            __size (int): length of one side, squared
-
-        """
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Args"""
-
-
-        if type(value) is not int:
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """Calulates area of square"""
-
-
-        area = self.__size * self.__size
-        return area
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
