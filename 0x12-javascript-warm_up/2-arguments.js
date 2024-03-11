a8a87a5778ae154f290d8de14b810f6584f9c441
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-11 18:29:46 +0100

    javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,11 @@ if (process.argv.length === 2)
 {
 	  console.log('No argument');
 }
-else (process.argv.length === 3)
+else if (process.argv.length === 3)
 {
 	  console.log('Argument found');
+}
+else
+{
+	  console.log('Arguments found');
 }
