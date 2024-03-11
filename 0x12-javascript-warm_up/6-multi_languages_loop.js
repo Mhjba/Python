b50cf6a30dad568d378c646c9393b83afd3e3173
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-11 19:42:19 +0100

    javscript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 // prints 3 lines
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-	  console.log(langs[i]);
+for (const lang of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+	  console.log(lang);
 }
