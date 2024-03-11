a3c5a6dd2932bbda73b7a2de914ca1f7401afad0
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-11 20:45:56 +0100

    javascript

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-// prints x times “C is fun”
-
-
-const x = Math.floor(Number(process.argv[2]));
+// script that prints x times “C is fun”
+const x = process.argv[2];
 if (isNaN(x)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i < x; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
