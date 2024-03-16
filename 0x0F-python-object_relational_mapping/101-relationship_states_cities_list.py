c37e08e4957bf091f525f2442ec259778b88b97b
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-16 13:20:01 +0000

    python object

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa"""
+""" script that lists all State objects,
+and corresponding City objects, contained in the database """
 
 from sys import argv
 from relationship_state import Base, State
@@ -10,9 +10,10 @@ from sqlalchemy.orm import Session
 from sqlalchemy.schema import Table
 
 if __name__ == "__main__":
+    # Create an engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2],
-                                   argv[3]), pool_pre_ping=True)
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Lists all City objects from the database hbtn_0e_101_usa
-"""
+""" script that lists all City objects from the database """
+
 from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
@@ -10,11 +9,14 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+    # Create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-
+    
+    # Create a configured
     Session = sessionmaker(bind=engine)
+    # Create a Session
     session = Session()
 
     st = session.query(State).join(City).order_by(City.id).all()
