984c0cf7ef8ab2cc69e9a4c55b8575bcfd940b22
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-16 11:15:07 +0000

    python object

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-Script that changes the name of a State object to the database
-Using module SQLAlchemy
+changes the name of a State object to the database
 """
 
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
 
 if __name__ == "__main__":
     # create an engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
     # create a configured "Session" class
     Session = sessionmaker(bind=engine)
     # create a Session
