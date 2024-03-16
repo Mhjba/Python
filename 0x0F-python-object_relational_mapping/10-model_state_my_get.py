2518db143cf892adea747ccd03aabcfada0fc295
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-16 10:21:54 +0000

    python object

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-script that prints the State object with the name passed
-as argument from the database 
+Prints the State object with the name passed as argument from the db
 """
 from sys import argv
 from model_state import Base, State
@@ -10,14 +9,10 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    # Create an engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-    
-    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
-    # Create a Session
     session = Session()
 
     state = session.query(State).filter(State.name == sys.argv[4]).first()
