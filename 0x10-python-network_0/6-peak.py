e1ae9430dd42ff8ec2dacf8484c55d059d9858c2
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-27 21:16:27 +0000

    python network 0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+""" Finds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
     """
     Args:
-        list_of_integers : list of integers to find peak of
+        list_of_integers: list of integers to find peak of
     Returns: peak of list_of_integers or None
     """
-
     size = list_of_integers
+    if size == []:
+        return None
     list = len(size)
-    if list == 0:
-        return (None)
+
+    list_of, mid = 0, list - 1
+    while list_of < mid:
+        mid_idx = list_of + (mid - list_of) // 2
+        if size[mid_idx] > size[mid_idx - 1] and size[mid_idx] > size[mid_idx + 1]:
+            return size[mid_idx]
+        if size[mid_idx - 1] > size[mid_idx + 1]:
+            mod = mid_idx
+        else:
+            list_of = mid_idx + 1
+    return size[list_of]
