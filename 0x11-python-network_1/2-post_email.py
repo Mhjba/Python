38b61053404f1709c5d0a4f0fe35d96100712f29
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-28 09:24:11 -0700

    python network 1

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+
+import urllib.parse as parse
+import urllib.request as req
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = parse.urlencode(value).encode("ascii")
+
+    request = req.Request(url, data)
+    with req.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
