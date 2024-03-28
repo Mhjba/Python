472925e5a5dcd1d08e3374eafb364faa0ec4331c
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-28 08:37:15 -0700

    python network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""that fetches https://alx-intranet.hbtn.io/status"""
+"Module that fetches https://alx-intranet.hbtn.io/status"
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    pack = response.read()
-    print('Body response:')
-    print('\t- type: {type(pack)}')
-    print('\t- content: {pack}')
-    print('\t- utf8 content: {pack.decode("utf-8")}')
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
