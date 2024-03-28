7f8b21c6dad24905b21c0c1cd2eb022af434de09
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-28 10:17:46 -0700

    python network 1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,17 +7,16 @@ with the letter as a parameter.
 import requests as req
 from sys import argv
 
-
 if __name__ == "__main__":
-    letter = "" if len(argv) == 1 else argv[1]
-    data = {"q": letter}
+    letter = argv[1] if len(argv) > 1 else ''
+    data={'q': letter}
 
-    res = req.post("http://0.0.0.0:5000/search_user", data=data)
     try:
-        j = res.json()
-        if j == {}:
-            print("No result")
+        res = req.post('http://0.0.0.0:5000/search_user')
+        json = res.json()
+        if json:
+            print('[{}] {}'.format(json['id'], json['name']))
         else:
-            print("[{}] {}".format(j("id"), j("name")))
+            print('No result')
     except ValueError:
-        print("Not a valid JSON")
+        print('Not a valid JSON')
