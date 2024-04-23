b9fa68ad81a6b8391ae2a0afc126bae7ea47ba6d
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-23 15:10:05 +0200

    js web

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,19 +3,19 @@ const request = require('request');
 
 const id_data = process.argv[2];
 const url_data = 'https://swapi-api.hbtn.io/api/films/';
-request.get(url_data + id_data, function (error, response, body) {
+request.get(url_data + id_data, function (error, esponse, body) {
   if (error) {
     console.log(error);
   }
   const data = JSON.parse(body);
   const todos = data.characters;
   for (const i of todos) {
-    request.get(i, function (error, response, bodys) {
+    request.get(i, function (error, esponse, bodys) {
       if (error) {
         console.log(error);
       }
-      const u_data = JSON.parse(bodys);
-      console.log(u_data.name);
+      const data1 = JSON.parse(bodys);
+      console.log(data1.name);
     });
   }
 });
