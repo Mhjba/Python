44b99bd4e2dc68e9bffffdb573e5467ca5864b4c
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-23 16:59:27 +0200

    js web

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,10 +2,10 @@
 const request = require('request');
 
 const process = require('process');
-const data_id = process.argv[2];
-const data_url = `https://swapi-api.alx-tools.com/api/films/${data_id}`;
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request(data_url, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   }
